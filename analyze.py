fcf5107470fa8240e0cd93c21172c9727ee7d389
phamdo <kaman@phamdo.com>
2016-05-08 12:20:44 -0400

    ratio of bipartisanship

--- analyze.py
+++ analyze.py
@@ -12,7 +12,6 @@ with open("data.json","r") as f:
 
 def bills_per_committee(data):
     size = 25
-
     com = {}
     passedcom = {}
     for session in data.values():
@@ -74,6 +73,7 @@ def cosponsors_v_pass(data):
     size = int(max(cosp_v_pass.keys())) + 1
     width = 1
     occurrences = [cosp_v_pass.get(key,0)/count for key in range(size)]
+    print occurrences
     num_cosp = range(size)
 
     fig, ax = plt.subplots()
@@ -87,7 +87,7 @@ def cosponsors_v_pass(data):
 
     plt.show()
 
-
+cosponsors_v_pass(data)
 def times(data):
     type_times = {k:{"count":0,"sum":0} for k in data['113'].keys()}
     for session in data.values():
@@ -98,8 +98,6 @@ def times(data):
                     start = datetime.fromtimestamp(time.mktime(time.strptime(bill['dates'][0],"%Y-%m-%d")))
                     end = datetime.fromtimestamp(time.mktime(time.strptime(bill['dates'][-1],"%Y-%m-%d")))
                     type_times[typ]["sum"] += (end - start).days
-
-
     size = len(type_times)
     width = 1
     types = type_times.keys()
@@ -165,7 +163,6 @@ def state_involvement(data):
                                                   
 
 
-
     counts = sorted(state_counts.values())
     counts = [x/total for x in counts]
     states = sorted(state_counts, key=state_counts.get)
@@ -221,12 +218,7 @@ def state_prob(data):
     ax.set_xticklabels(states)
 
     plt.show()
-
                 
-
-
-
-
 def amendments(data):
     amendments = {k:{"days":0,"passed":0,"count":0} for k in range(1000)}
     maximal = 0
@@ -271,4 +263,70 @@ def amendments(data):
 
     plt.show()
 
-state_prob(data)
+
+from math import log
+#100% bipartisan if 50% R and 50% D, 0% partisan if 100% R or 100% D
+#binary entropy
+def bipartisanship(R, D):
+    p =  R / float(D + R)
+    if p == 1:
+        return 0
+    entropy = - p * log(p, 2) - (1-p)*log((1-p), 2)
+    print entropy
+    return entropy
+
+#bipartisanship of cosponsors vs likelihood of bill passing
+#only examines bills with at least 1 cosponsor
+def bipart(data):
+    x = {} #bills passed
+    y = {} #bills not passed
+    ratios = {0.0: "0/100", 0.1: "10/90", 0.2: "20/80", 0.3: "30/70", 0.4: "40/60", 0.5: "50/50", 0.6: "60/40", 0.7: "70/30", 0.8: "80/20", 0.9: "90/10", 1.0: "100/0"}
+    for session in data.values():
+        for typ in session.values():
+            for bill in typ.values():
+                r = 0
+                d = 0
+                for s in bill['cosponsors']:
+                    #print s
+                    if s['party'] == 'R':
+                        r += 1
+                    else:
+                        d += 1
+                    ratio = r / float(d + r)
+                if "becamePublicLaw" in bill['actions']:
+                    pass
+                if d + r != 0:
+                    rat = round((r / float(d+r)), 1) #approx % of rep
+                    ratio = ratios[rat]
+                    if "becamePublicLaw" in bill['actions']:
+                        if ratio in x.keys():
+                            x[ratio] += 1
+                        else:
+                            x[ratio] = 1
+                    else:
+                        if ratio in y.keys():
+                            y[ratio] += 1
+                        else:
+                            y[ratio] = 1
+    size = len(x)
+    width = 1
+    types = sorted(x)
+    del types[2]
+    types.append("100/0")
+    result = []
+    
+    for i in types:
+        num = round(x[i]/float(x[i] + y[i]), 5)
+        result.append(num * 100)
+    
+    fig, ax = plt.subplots()
+    chart1 = ax.bar(np.arange(size), result, width)
+    ax.set_xticks(np.arange(size) + .5)
+    ax.set_xticklabels(types)
+    fig.canvas.set_window_title("bipartisanship_vs_likelihood_to_pass")
+    ax.set_xlabel("% Republican Cosponsors/% Democrat Cosponsors")
+    ax.set_ylabel("% bills passed")
+    plt.show()
+
+
+bipart(data)
